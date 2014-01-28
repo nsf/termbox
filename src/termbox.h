75f3ce320b326fcc8b744f1398d5b89812c1e949
nsf <no.smile.face@gmail.com>
2014-01-28 23:05:44 +0600

    Realign defines with spaces instead of tabs.
    As a side note, it makes sense to convert the preprocessor defines to enums
    eventually. Better for parsing tools.

--- src/termbox.h
+++ src/termbox.h
@@ -18,75 +18,75 @@ extern "C" {
 
 // These are a safe subset of terminfo keys, which exist on all popular
 // terminals. Termbox uses only them to stay truly portable.
-#define TB_KEY_F1		(0xFFFF-0)
-#define TB_KEY_F2		(0xFFFF-1)
-#define TB_KEY_F3		(0xFFFF-2)
-#define TB_KEY_F4		(0xFFFF-3)
-#define TB_KEY_F5		(0xFFFF-4)
-#define TB_KEY_F6		(0xFFFF-5)
-#define TB_KEY_F7		(0xFFFF-6)
-#define TB_KEY_F8		(0xFFFF-7)
-#define TB_KEY_F9		(0xFFFF-8)
-#define TB_KEY_F10		(0xFFFF-9)
-#define TB_KEY_F11		(0xFFFF-10)
-#define TB_KEY_F12		(0xFFFF-11)
-#define TB_KEY_INSERT		(0xFFFF-12)
-#define TB_KEY_DELETE		(0xFFFF-13)
-#define TB_KEY_HOME		(0xFFFF-14)
-#define TB_KEY_END		(0xFFFF-15)
-#define TB_KEY_PGUP		(0xFFFF-16)
-#define TB_KEY_PGDN		(0xFFFF-17)
-#define TB_KEY_ARROW_UP		(0xFFFF-18)
-#define TB_KEY_ARROW_DOWN	(0xFFFF-19)
-#define TB_KEY_ARROW_LEFT	(0xFFFF-20)
-#define TB_KEY_ARROW_RIGHT	(0xFFFF-21)
+#define TB_KEY_F1          (0xFFFF-0)
+#define TB_KEY_F2          (0xFFFF-1)
+#define TB_KEY_F3          (0xFFFF-2)
+#define TB_KEY_F4          (0xFFFF-3)
+#define TB_KEY_F5          (0xFFFF-4)
+#define TB_KEY_F6          (0xFFFF-5)
+#define TB_KEY_F7          (0xFFFF-6)
+#define TB_KEY_F8          (0xFFFF-7)
+#define TB_KEY_F9          (0xFFFF-8)
+#define TB_KEY_F10         (0xFFFF-9)
+#define TB_KEY_F11         (0xFFFF-10)
+#define TB_KEY_F12         (0xFFFF-11)
+#define TB_KEY_INSERT      (0xFFFF-12)
+#define TB_KEY_DELETE      (0xFFFF-13)
+#define TB_KEY_HOME        (0xFFFF-14)
+#define TB_KEY_END         (0xFFFF-15)
+#define TB_KEY_PGUP        (0xFFFF-16)
+#define TB_KEY_PGDN        (0xFFFF-17)
+#define TB_KEY_ARROW_UP    (0xFFFF-18)
+#define TB_KEY_ARROW_DOWN  (0xFFFF-19)
+#define TB_KEY_ARROW_LEFT  (0xFFFF-20)
+#define TB_KEY_ARROW_RIGHT (0xFFFF-21)
 
 // These are all ASCII code points below SPACE character and a BACKSPACE key.
-#define TB_KEY_CTRL_TILDE	0x00
-#define TB_KEY_CTRL_2		0x00 // clash with 'CTRL_TILDE'
-#define TB_KEY_CTRL_A		0x01
-#define TB_KEY_CTRL_B		0x02
-#define TB_KEY_CTRL_C		0x03
-#define TB_KEY_CTRL_D		0x04
-#define TB_KEY_CTRL_E		0x05
-#define TB_KEY_CTRL_F		0x06
-#define TB_KEY_CTRL_G		0x07
-#define TB_KEY_BACKSPACE	0x08
-#define TB_KEY_CTRL_H		0x08 // clash with 'CTRL_BACKSPACE'
-#define TB_KEY_TAB		0x09
-#define TB_KEY_CTRL_I		0x09 // clash with 'TAB'
-#define TB_KEY_CTRL_J		0x0A
-#define TB_KEY_CTRL_K		0x0B
-#define TB_KEY_CTRL_L		0x0C
-#define TB_KEY_ENTER		0x0D
-#define TB_KEY_CTRL_M		0x0D // clash with 'ENTER'
-#define TB_KEY_CTRL_N		0x0E
-#define TB_KEY_CTRL_O		0x0F
-#define TB_KEY_CTRL_P		0x10
-#define TB_KEY_CTRL_Q		0x11
-#define TB_KEY_CTRL_R		0x12
-#define TB_KEY_CTRL_S		0x13
-#define TB_KEY_CTRL_T		0x14
-#define TB_KEY_CTRL_U		0x15
-#define TB_KEY_CTRL_V		0x16
-#define TB_KEY_CTRL_W		0x17
-#define TB_KEY_CTRL_X		0x18
-#define TB_KEY_CTRL_Y		0x19
-#define TB_KEY_CTRL_Z		0x1A
-#define TB_KEY_ESC		0x1B
-#define TB_KEY_CTRL_LSQ_BRACKET	0x1B // clash with 'ESC'
-#define TB_KEY_CTRL_3		0x1B // clash with 'ESC'
-#define TB_KEY_CTRL_4		0x1C
-#define TB_KEY_CTRL_BACKSLASH	0x1C // clash with 'CTRL_4'
-#define TB_KEY_CTRL_5		0x1D
+#define TB_KEY_CTRL_TILDE       0x00
+#define TB_KEY_CTRL_2           0x00 // clash with 'CTRL_TILDE'
+#define TB_KEY_CTRL_A           0x01
+#define TB_KEY_CTRL_B           0x02
+#define TB_KEY_CTRL_C           0x03
+#define TB_KEY_CTRL_D           0x04
+#define TB_KEY_CTRL_E           0x05
+#define TB_KEY_CTRL_F           0x06
+#define TB_KEY_CTRL_G           0x07
+#define TB_KEY_BACKSPACE        0x08
+#define TB_KEY_CTRL_H           0x08 // clash with 'CTRL_BACKSPACE'
+#define TB_KEY_TAB              0x09
+#define TB_KEY_CTRL_I           0x09 // clash with 'TAB'
+#define TB_KEY_CTRL_J           0x0A
+#define TB_KEY_CTRL_K           0x0B
+#define TB_KEY_CTRL_L           0x0C
+#define TB_KEY_ENTER            0x0D
+#define TB_KEY_CTRL_M           0x0D // clash with 'ENTER'
+#define TB_KEY_CTRL_N           0x0E
+#define TB_KEY_CTRL_O           0x0F
+#define TB_KEY_CTRL_P           0x10
+#define TB_KEY_CTRL_Q           0x11
+#define TB_KEY_CTRL_R           0x12
+#define TB_KEY_CTRL_S           0x13
+#define TB_KEY_CTRL_T           0x14
+#define TB_KEY_CTRL_U           0x15
+#define TB_KEY_CTRL_V           0x16
+#define TB_KEY_CTRL_W           0x17
+#define TB_KEY_CTRL_X           0x18
+#define TB_KEY_CTRL_Y           0x19
+#define TB_KEY_CTRL_Z           0x1A
+#define TB_KEY_ESC              0x1B
+#define TB_KEY_CTRL_LSQ_BRACKET 0x1B // clash with 'ESC'
+#define TB_KEY_CTRL_3           0x1B // clash with 'ESC'
+#define TB_KEY_CTRL_4           0x1C
+#define TB_KEY_CTRL_BACKSLASH   0x1C // clash with 'CTRL_4'
+#define TB_KEY_CTRL_5           0x1D
 #define TB_KEY_CTRL_RSQ_BRACKET 0x1D // clash with 'CTRL_5'
-#define TB_KEY_CTRL_6		0x1E
-#define TB_KEY_CTRL_7		0x1F
-#define TB_KEY_CTRL_SLASH	0x1F // clash with 'CTRL_7'
-#define TB_KEY_CTRL_UNDERSCORE	0x1F // clash with 'CTRL_7'
-#define TB_KEY_SPACE		0x20
-#define TB_KEY_BACKSPACE2	0x7F
-#define TB_KEY_CTRL_8		0x7F // clash with 'DELETE'
+#define TB_KEY_CTRL_6           0x1E
+#define TB_KEY_CTRL_7           0x1F
+#define TB_KEY_CTRL_SLASH       0x1F // clash with 'CTRL_7'
+#define TB_KEY_CTRL_UNDERSCORE  0x1F // clash with 'CTRL_7'
+#define TB_KEY_SPACE            0x20
+#define TB_KEY_BACKSPACE2       0x7F
+#define TB_KEY_CTRL_8           0x7F // clash with 'DELETE'
 
 // These are non-existing ones.
 //
@@ -96,26 +96,26 @@ extern "C" {
 
 // Currently there is only one modificator. See also struct tb_event's mod
 // field.
-#define TB_MOD_ALT	0x01
+#define TB_MOD_ALT 0x01
 
 // Colors (see struct tb_cell's fg and bg fields).
-#define TB_DEFAULT	0x00
-#define TB_BLACK	0x01
-#define TB_RED		0x02
-#define TB_GREEN	0x03
-#define TB_YELLOW	0x04
-#define TB_BLUE		0x05
-#define TB_MAGENTA	0x06
-#define TB_CYAN		0x07
-#define TB_WHITE	0x08
+#define TB_DEFAULT 0x00
+#define TB_BLACK   0x01
+#define TB_RED     0x02
+#define TB_GREEN   0x03
+#define TB_YELLOW  0x04
+#define TB_BLUE    0x05
+#define TB_MAGENTA 0x06
+#define TB_CYAN    0x07
+#define TB_WHITE   0x08
 
 // Attributes, it is possible to use multiple attributes by combining them
 // using bitwise OR ('|'). Although, colors cannot be combined. But you can
 // combine attributes and a single color. See also struct tb_cell's fg and bg
 // fields.
-#define TB_BOLD		0x10
-#define TB_UNDERLINE	0x20
-#define TB_REVERSE	0x40
+#define TB_BOLD      0x10
+#define TB_UNDERLINE 0x20
+#define TB_REVERSE   0x40
 
 // A cell, single conceptual entity on the terminal screen. The terminal screen
 // is basically a 2d array of cells. It has the following fields:
@@ -128,7 +128,7 @@ struct tb_cell {
 	uint16_t bg;
 };
 
-#define TB_EVENT_KEY 1
+#define TB_EVENT_KEY    1
 #define TB_EVENT_RESIZE 2
 
 // This struct represents a termbox event. The 'mod', 'key' and 'ch' fields are
@@ -147,9 +147,9 @@ struct tb_event {
 // the pipe trap error. Termbox uses unix pipes in order to deliever a message
 // from a signal handler (SIGWINCH) to the main event reading loop. Honestly in
 // most cases you should just check the returned code as < 0.
-#define TB_EUNSUPPORTED_TERMINAL	-1
-#define TB_EFAILED_TO_OPEN_TTY		-2
-#define TB_EPIPE_TRAP_ERROR		-3
+#define TB_EUNSUPPORTED_TERMINAL -1
+#define TB_EFAILED_TO_OPEN_TTY   -2
+#define TB_EPIPE_TRAP_ERROR      -3
 
 // Initializes the termbox library. This function should be called before any
 // other functions. After successful initialization, the library must be
@@ -189,8 +189,8 @@ SO_IMPORT void tb_change_cell(int x, int y, uint32_t ch, uint16_t fg, uint16_t b
 SO_IMPORT void tb_blit(int x, int y, int w, int h, const struct tb_cell *cells);
 
 #define TB_INPUT_CURRENT 0
-#define TB_INPUT_ESC 1
-#define TB_INPUT_ALT 2
+#define TB_INPUT_ESC     1
+#define TB_INPUT_ALT     2
 
 // Sets the termbox input mode. Termbox has two input modes:
 // 1. Esc input mode.
@@ -203,10 +203,10 @@ SO_IMPORT void tb_blit(int x, int y, int w, int h, const struct tb_cell *cells);
 // If 'mode' is TB_INPUT_CURRENT, it returns the current input mode.
 SO_IMPORT int tb_select_input_mode(int mode);
 
-#define TB_OUTPUT_CURRENT 0
-#define TB_OUTPUT_NORMAL 1
-#define TB_OUTPUT_256 2
-#define TB_OUTPUT_216 3
+#define TB_OUTPUT_CURRENT   0
+#define TB_OUTPUT_NORMAL    1
+#define TB_OUTPUT_256       2
+#define TB_OUTPUT_216       3
 #define TB_OUTPUT_GRAYSCALE 4
 
 // Sets the termbox output mode. Termbox has three output options:
