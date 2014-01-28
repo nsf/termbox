d69b41c7be6c0db2e089284bd9dab350ef5658dd
nsf <no.smile.face@gmail.com>
2014-01-28 22:53:09 +0600

    Update python bindings.
    Output mode functions/constants. Realign things using spaces instead of tabs.

--- src/python/termboxmodule.pyx
+++ src/python/termboxmodule.pyx
@@ -26,7 +26,8 @@ cdef extern from "../termbox.h":
 	int tb_width()
 	int tb_height()
 	void tb_set_cursor(int x, int y)
-	void tb_select_input_mode(int mode)
+	int tb_select_input_mode(int mode)
+	int tb_select_output_mode(int mode)
 	int tb_peek_event(tb_event *event, int timeout) nogil
 	int tb_poll_event(tb_event *event) nogil
 
@@ -41,100 +42,105 @@ import threading
 __instance = None
 
 # keys ----------------------------------
-KEY_F1			= (0xFFFF-0)
-KEY_F2			= (0xFFFF-1)
-KEY_F3			= (0xFFFF-2)
-KEY_F4			= (0xFFFF-3)
-KEY_F5			= (0xFFFF-4)
-KEY_F6			= (0xFFFF-5)
-KEY_F7			= (0xFFFF-6)
-KEY_F8			= (0xFFFF-7)
-KEY_F9			= (0xFFFF-8)
-KEY_F10			= (0xFFFF-9)
-KEY_F11			= (0xFFFF-10)
-KEY_F12			= (0xFFFF-11)
-KEY_INSERT		= (0xFFFF-12)
-KEY_DELETE		= (0xFFFF-13)
-KEY_HOME		= (0xFFFF-14)
-KEY_END			= (0xFFFF-15)
-KEY_PGUP		= (0xFFFF-16)
-KEY_PGDN		= (0xFFFF-17)
-KEY_ARROW_UP		= (0xFFFF-18)
-KEY_ARROW_DOWN		= (0xFFFF-19)
-KEY_ARROW_LEFT		= (0xFFFF-20)
-KEY_ARROW_RIGHT		= (0xFFFF-21)
+KEY_F1               = (0xFFFF-0)
+KEY_F2               = (0xFFFF-1)
+KEY_F3               = (0xFFFF-2)
+KEY_F4               = (0xFFFF-3)
+KEY_F5               = (0xFFFF-4)
+KEY_F6               = (0xFFFF-5)
+KEY_F7               = (0xFFFF-6)
+KEY_F8               = (0xFFFF-7)
+KEY_F9               = (0xFFFF-8)
+KEY_F10              = (0xFFFF-9)
+KEY_F11              = (0xFFFF-10)
+KEY_F12              = (0xFFFF-11)
+KEY_INSERT           = (0xFFFF-12)
+KEY_DELETE           = (0xFFFF-13)
+KEY_HOME             = (0xFFFF-14)
+KEY_END              = (0xFFFF-15)
+KEY_PGUP             = (0xFFFF-16)
+KEY_PGDN             = (0xFFFF-17)
+KEY_ARROW_UP         = (0xFFFF-18)
+KEY_ARROW_DOWN       = (0xFFFF-19)
+KEY_ARROW_LEFT       = (0xFFFF-20)
+KEY_ARROW_RIGHT      = (0xFFFF-21)
+KEY_CTRL_TILDE       = 0x00
+KEY_CTRL_2           = 0x00
+KEY_CTRL_A           = 0x01
+KEY_CTRL_B           = 0x02
+KEY_CTRL_C           = 0x03
+KEY_CTRL_D           = 0x04
+KEY_CTRL_E           = 0x05
+KEY_CTRL_F           = 0x06
+KEY_CTRL_G           = 0x07
+KEY_BACKSPACE        = 0x08
+KEY_CTRL_H           = 0x08
+KEY_TAB              = 0x09
+KEY_CTRL_I           = 0x09
+KEY_CTRL_J           = 0x0A
+KEY_CTRL_K           = 0x0B
+KEY_CTRL_L           = 0x0C
+KEY_ENTER            = 0x0D
+KEY_CTRL_M           = 0x0D
+KEY_CTRL_N           = 0x0E
+KEY_CTRL_O           = 0x0F
+KEY_CTRL_P           = 0x10
+KEY_CTRL_Q           = 0x11
+KEY_CTRL_R           = 0x12
+KEY_CTRL_S           = 0x13
+KEY_CTRL_T           = 0x14
+KEY_CTRL_U           = 0x15
+KEY_CTRL_V           = 0x16
+KEY_CTRL_W           = 0x17
+KEY_CTRL_X           = 0x18
+KEY_CTRL_Y           = 0x19
+KEY_CTRL_Z           = 0x1A
+KEY_ESC              = 0x1B
+KEY_CTRL_LSQ_BRACKET = 0x1B
+KEY_CTRL_3           = 0x1B
+KEY_CTRL_4           = 0x1C
+KEY_CTRL_BACKSLASH   = 0x1C
+KEY_CTRL_5           = 0x1D
+KEY_CTRL_RSQ_BRACKET = 0x1D
+KEY_CTRL_6           = 0x1E
+KEY_CTRL_7           = 0x1F
+KEY_CTRL_SLASH       = 0x1F
+KEY_CTRL_UNDERSCORE  = 0x1F
+KEY_SPACE            = 0x20
+KEY_BACKSPACE2       = 0x7F
+KEY_CTRL_8           = 0x7F
 
-KEY_CTRL_TILDE		= 0x00
-KEY_CTRL_2		= 0x00
-KEY_CTRL_A		= 0x01
-KEY_CTRL_B		= 0x02
-KEY_CTRL_C		= 0x03
-KEY_CTRL_D		= 0x04
-KEY_CTRL_E		= 0x05
-KEY_CTRL_F		= 0x06
-KEY_CTRL_G		= 0x07
-KEY_BACKSPACE		= 0x08
-KEY_CTRL_H		= 0x08
-KEY_TAB			= 0x09
-KEY_CTRL_I		= 0x09
-KEY_CTRL_J		= 0x0A
-KEY_CTRL_K		= 0x0B
-KEY_CTRL_L		= 0x0C
-KEY_ENTER		= 0x0D
-KEY_CTRL_M		= 0x0D
-KEY_CTRL_N		= 0x0E
-KEY_CTRL_O		= 0x0F
-KEY_CTRL_P		= 0x10
-KEY_CTRL_Q		= 0x11
-KEY_CTRL_R		= 0x12
-KEY_CTRL_S		= 0x13
-KEY_CTRL_T		= 0x14
-KEY_CTRL_U		= 0x15
-KEY_CTRL_V		= 0x16
-KEY_CTRL_W		= 0x17
-KEY_CTRL_X		= 0x18
-KEY_CTRL_Y		= 0x19
-KEY_CTRL_Z		= 0x1A
-KEY_ESC			= 0x1B
-KEY_CTRL_LSQ_BRACKET	= 0x1B
-KEY_CTRL_3		= 0x1B
-KEY_CTRL_4		= 0x1C
-KEY_CTRL_BACKSLASH	= 0x1C
-KEY_CTRL_5		= 0x1D
-KEY_CTRL_RSQ_BRACKET	= 0x1D
-KEY_CTRL_6		= 0x1E
-KEY_CTRL_7		= 0x1F
-KEY_CTRL_SLASH		= 0x1F
-KEY_CTRL_UNDERSCORE	= 0x1F
-KEY_SPACE		= 0x20
-KEY_BACKSPACE2		= 0x7F
-KEY_CTRL_8		= 0x7F
-
-MOD_ALT			= 0x01
+MOD_ALT              = 0x01
 
 # attributes ----------------------
 
-DEFAULT		= 0x00
-BLACK		= 0x01
-RED		= 0x02
-GREEN		= 0x03
-YELLOW		= 0x04
-BLUE		= 0x05
-MAGENTA		= 0x06
-CYAN		= 0x07
-WHITE		= 0x08
+DEFAULT   = 0x00
+BLACK     = 0x01
+RED       = 0x02
+GREEN     = 0x03
+YELLOW    = 0x04
+BLUE      = 0x05
+MAGENTA   = 0x06
+CYAN      = 0x07
+WHITE     = 0x08
 
-BOLD		= 0x10
-UNDERLINE	= 0x20
-REVERSE		= 0x40
+BOLD      = 0x10
+UNDERLINE = 0x20
+REVERSE   = 0x40
 
 # misc ----------------------------
 
-HIDE_CURSOR	= -1
-INPUT_ESC	= 1
-INPUT_ALT	= 2
-EVENT_KEY	= 1
-EVENT_RESIZE	= 2
+HIDE_CURSOR      = -1
+INPUT_CURRENT    = 0
+INPUT_ESC        = 1
+INPUT_ALT        = 2
+OUTPUT_CURRENT   = 0
+OUTPUT_NORMAL    = 1
+OUTPUT_256       = 2
+OUTPUT_216       = 3
+OUTPUT_GRAYSCALE = 4
+EVENT_KEY        = 1
+EVENT_RESIZE     = 2
 
 cdef class Termbox:
 	cdef int created
@@ -216,8 +222,17 @@ cdef class Termbox:
 
 	def select_input_mode(self, int mode):
 		"""Select preferred input mode: INPUT_ESC or INPUT_ALT.
+
+		   INPUT_CURRENT returns the selected mode without changing anything.
 		"""
-		tb_select_input_mode(mode)
+		return int(tb_select_input_mode(mode))
+
+	def select_output_mode(self, int mode):
+		"""Select preferred output mode: one of OUTPUT_* constants.
+
+		   OUTPUT_CURRENT returns the selected mode without changing anything.
+		"""
+		return int(tb_select_output_mode(mode))
 
 	def peek_event(self, int timeout=0):
 		"""Wait for an event up to 'timeout' milliseconds and return it.
